fcdea444b6c7d1690f616a2c7f988eb3d434a125
therealmbittarelli <maggiekmcclellan@gmail.com>
2020-02-12 21:53:23 -0500

    pantry POST and PATCH now working

--- src/pantry/pantry-router.js
+++ src/pantry/pantry-router.js
@@ -9,71 +9,79 @@ const path = require("path");
 const bodyParser = express.json();
 const pantryRouter = express.Router();
 
-// need to build protected endpoints here and in recipe router
+const serializeIngredient = (ingredient) => {
+  return {
+    ...ingredient
+  };
+};
 
 pantryRouter
   .route("/")
   .get(requireAuth, (req, res, next) => {
     console.log("pantry ingredients user id GET req is", req.user.id);
     let user_id = req.user.id;
-    PantryService.getIngredients(req.app.get('db'), user_id) // need to pull this data in from req once we know what is being sent from client
+    PantryService.getIngredients(req.app.get("db"), user_id)
       .then((ingredients) => {
-        console.log('ingredients list is', ingredients);
-        res.json(ingredients);
+        console.log("ingredients list is", ingredients);
+        res
+          .status(200)
+          .json(ingredients);
       })
       .catch((err) => {
         next(err);
       });
   })
 
-// haven't been able to test POST at all yet
-//   .post(requireAuth, bodyParser, (req, res, next) => {
-//     console.log('ingredient POST req.body is', req.body);
-//     const newIngredient = { ingredient_name: , in_stock: , notes: , ingredient_owner: }
+  .post(requireAuth, bodyParser, (req, res, next) => {
+    console.log("ingredient POST req.body is", req.body);
+    let { ingredient_name, in_stock, notes } = req.body;
+    let ingredient_owner = req.user.id;
+    const newIngredient = { ingredient_name, in_stock, notes, ingredient_owner };
+    console.log("new ingredient from req is", newIngredient);
+    for (const [key, value] of Object.entries(newIngredient)) {
+      if (value === null) {
+        return res.status(400).json({
+          error: { message: `Missing ${key} in request body` }
+        });
+      }
+    }
+    PantryService.addIngredient(req.app.get("db"), newIngredient)
+      .then(ingredient => {
+        console.log("res is", serializeIngredient(ingredient));
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${ingredient.id}`))
+          .json(serializeIngredient(ingredient));
+      })
+      .catch((err) => {
+        next(err);
+      });
+  });
 
-//     for (const [key, value] of Object.entries(newIngredient)) {
-//       if (value === null) {
-//         return res.status(400).json({
-//           error: { message: `Missing ${key} in request body` }
-//         });
-//       }
-//     }
-//     PantryService.addIngredient(req.app.get('db'), newIngredient)
-//       .then(ingredient => {
-//         res
-//           .status(201)
-//           .location(path.posix.join(req.originalUrl, `/${ingredient.id}`))
-//           .json({
-//             ingredient_name: ingredient.ingredient_name,
-//             in_stock: ingredient.in_stock,
-//             notes: ingredient.notes,
-//             ingredient_owner: ingredient.ingredient_owner
-//           });
-//       })
-//       .catch((err) => {
-//         next(err);
-//       });
-//   });
-
-// pantryRouter
-//   .route('/:ingredient_id')
-//   .patch(requireAuth, bodyParser, (req, res, next) => {
-//     let updatedIngredient = req.body.updatedIngredient
-//     PantryService.updateIngredient(req.app.get('db'), updatedIngredient)
-//       .then((updatedIngredientResponse) => {
-//         res
-//           .status(201)
-//           .json({
-//             ingredient_name: updatedIngredientResponse.ingredient_name,
-//             in_stock: updatedIngredientResponse.in_stock,
-//             notes: updatedIngredientResponse.notes,
-//             ingredient_owner: updatedIngredientResponse.ingredient_owner
-//           });
-//       })
-//       .catch((err) => {
-//         next(err);
-//       });
-//   });
+pantryRouter
+  .route("/:ingredient_id")
+  .patch(requireAuth, bodyParser, (req, res, next) => {
+    let { ingredient_name, in_stock, notes } = req.body;
+    let updatedIngredient = { ingredient_name, in_stock, notes };
+    let ingredientId = req.body.id
+    console.log('updatedIngredient is', updatedIngredient);
+    console.log('req PATCH is', req);
+    PantryService.updateIngredient(req.app.get("db"), updatedIngredient, ingredientId)
+      .then((updatedIngredientResponse) => {
+        console.log('updatedPatch is', updatedIngredientResponse);
+        res
+          .status(201)
+          .json({
+            ingredient_name: updatedIngredientResponse.ingredient_name,
+            in_stock: updatedIngredientResponse.in_stock,
+            notes: updatedIngredientResponse.notes,
+            ingredient_owner: updatedIngredientResponse.ingredient_owner
+          });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  });
 
 
 
